41f447a638153c439c328bc4e728b4bd4cdf060b
adnensaid83 <adnensaid83@gmail.com>
2026-01-28 16:38:08 +0100

    add routes to app ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import GaleryPage from './pages/GaleryPage';
+import HomePage from './pages/HomePage';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 class="bg-primary">color primary</h1>
-      <h1 class="bg-secondary">color secondary</h1>
-      <h1 class="bg-tertiary">color tertiary</h1>
-      <h1 class="bg-gradient">color gradient</h1>
-      <h1 class="bg-accent">color accent</h1>
-      <h1 class="bg-bg">color bg</h1>
-      <h1 class="bg-white">color white</h1>
-      <h1 class="bg-muted">color muted</h1>
-      <h1 class="bg-subtitle">color subtitle</h1>
-      <h1 class="font-default">font</h1>
-      <h1 class="font-normal">weight</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/movies" element={<GaleryPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <h1 className="text-5xl bg-primary text-red">Home page</h1>;
+}
+
+export default HomePage;
