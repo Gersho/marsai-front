cf1f55bbf3825bd117c2d1dd8b0dfee85045b8f9
Karim Pierre Zennoune <karim-pierre.zennoune@laplateforme.io>
2026-01-28 16:38:54 +0100

    add missing packages for lint (eslint-config-prettier and eslint-plugin-react)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 class="bg-primary">color primary</h1>
-      <h1 class="bg-secondary">color secondary</h1>
-      <h1 class="bg-tertiary">color tertiary</h1>
-      <h1 class="bg-gradient">color gradient</h1>
-      <h1 class="bg-accent">color accent</h1>
-      <h1 class="bg-bg">color bg</h1>
-      <h1 class="bg-white">color white</h1>
-      <h1 class="bg-muted">color muted</h1>
-      <h1 class="bg-subtitle">color subtitle</h1>
-      <h1 class="font-default">font</h1>
-      <h1 class="font-normal">weight</h1>
+      <h1 className="bg-primary">color primary</h1>
+      <h1 className="bg-secondary">color secondary</h1>
+      <h1 className="bg-tertiary">color tertiary</h1>
+      <h1 className="bg-gradient">color gradient</h1>
+      <h1 className="bg-accent">color accent</h1>
+      <h1 className="bg-bg">color bg</h1>
+      <h1 className="bg-white">color white</h1>
+      <h1 className="bg-muted">color muted</h1>
+      <h1 className="bg-subtitle">color subtitle</h1>
+      <h1 className="font-default">font</h1>
+      <h1 className="font-normal">weight</h1>
 
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -40,7 +40,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
