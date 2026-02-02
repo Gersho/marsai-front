e8e65e07158cb587c644713a83a73fb202026f4c
adnensaid83 <adnensaid83@gmail.com>
2026-02-02 12:35:45 +0100

    add pages

---
+++ src/pages/GaleryPage.jsx
@@ -0,0 +1,9 @@
+function GaleryPage() {
+  return (
+    <div className="typography">
+      <h1>Galery page</h1>
+    </div>
+  );
+}
+
+export default GaleryPage;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+function HomePage() {
+  return (
+    <div className="typography min-h-[200vh] bg-black bg-[url(/src/assets/banner.png)] bg-no-repeat bg-cover"></div>
+  );
+}
+
+export default HomePage;
