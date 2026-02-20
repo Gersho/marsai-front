8d00d2521724a5e57117ed0a9bd2cb1bc71156e7
Karim Pierre Zennoune <karim-pierre.zennoune@laplateforme.io>
2026-02-20 13:28:14 +0100

    added pagination

---
+++ src/components/base/PaginationMenu.jsx
@@ -0,0 +1,44 @@
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+
+
+function PaginationMenu({
+    page,
+    setPage,
+    total,
+    perPage = 20
+}) {
+    function incrementPage() {
+        setPage(page + 1);
+    }
+
+    function decrementPage() {
+        if (page > 1) {
+            setPage(page - 1);
+        }
+    }
+
+    return (
+        <div className='flex flex-row gap-x-4 pb-4'>
+            {
+                page !== 1 ?
+                    <div className=""
+                        onClick={decrementPage}
+                    ><IoIosArrowBack size={36} /></div>
+                    : <></>
+            }
+            {page !== 1 ? <div onClick={decrementPage} className='border border-sm rounded-md min-w-10 text-center pt-1 bg-primary' > {page - 1} </div> : <></>}
+            <div className='border border-sm rounded-md min-w-10 text-center pt-1 bg-accent' > {page} </div>
+            {page * perPage < total ? <div onClick={incrementPage} className='border border-sm rounded-md min-w-10 text-center pt-1 bg-primary' > {page + 1} </div> : <></>}
+            {
+                page * perPage < total ?
+                    <div
+                        onClick={incrementPage}
+                    ><IoIosArrowForward size={36} /></div>
+                    : <></>
+            }
+        </div>
+    );
+}
+
+export default PaginationMenu;
