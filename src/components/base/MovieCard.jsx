2cce4f98b7f9affc55b896accd75f5d3c9792c9b
adnensaid83 <adnensaid83@gmail.com>
2026-02-26 11:54:15 +0100

    galerie page integration ok

--- src/components/base/MovieCard.jsx
+++ src/components/base/MovieCard.jsx
@@ -20,52 +20,43 @@ function MovieCard({ data }) {
   let director = data.director.firstname + ' ' + data.director.lastname;
 
   return (
-    <div className="flex flex-col uppercase w-10/12 rounded-xl bg-primary sm:w-42 md:w-60 md:max-w-1/5">
+    <div className="flex flex-col uppercase rounded-xl ">
       <div className="relative">
         <img
-          className={`aspect-video w-full  object-contain rounded-t-xl bg-dark`}
+          className={`aspect-video w-full object-cover rounded-t-xl bg-primary`}
           src={data.cover_path}
         />
-        <span
-          className=" bg-secondary
-                    absolute
-                   right-1
-                    bottom-1
-                    text-center
-                    text-xs
-                    px-1
-                    rounded-md
-                    lowercase
-                   "
-        >
+        <span className="bg-secondary text-white absolute right-1 bottom-1 text-center text-xs px-4 rounded-md lowercase">
           {data.duration} s
         </span>
-        <span className=" bg-secondary absolute text-center text-xs px-1 rounded-md left-1 top-1">
+        <span className=" bg-white absolute text-center text-[10px] px-2 rounded-md left-1 top-2">
           {data.is_hybrid ? 'Hybrid' : 'Full-AI'}
         </span>
       </div>
-
-      <div className="px-2 pt-1">
+      <div className="p-2 bg-primary rounded-b-2xl">
         <div className="flex flex-row justify-between ">
           <div className="flex flex-col  min-w-0 max-w-100">
             <p
-              className="text-md font-bold truncate "
+              className="text-lg font-bold truncate text-white"
               title={data.english_title}
             >
               {english_title}
             </p>
-            <p className="text-sm truncate" title={data.original_title}>
+            <p
+              className="text-xs truncate text-dark"
+              title={data.original_title}
+            >
               {original_title}
             </p>
           </div>
         </div>
         <div className="flex flex-row justify-between">
-          <div className="flex flex-col min-w-0 max-w-100">
-            <p className="text-xs truncate pt-1">{director}</p>
+          <div className="flex flex-col ">
+            <p className=" text-dark">{director}</p>
           </div>
           <div className="flex flex-col ">
             <p
-              className="text-xl pb-2"
+              className="text-base text-white"
               title={i18n.language === 'fr' ? language.textFr : language.textEn}
             >
               {language.icon}{' '}
