9ed5da0a258a09b48ce39dcbd5a0eeb537ef02a9
Karim Pierre Zennoune <karim-pierre.zennoune@laplateforme.io>
2026-03-18 13:35:06 +0100

    getmovie data fixed

--- src/pages/EditMoviePage.jsx
+++ src/pages/EditMoviePage.jsx
@@ -1,62 +1,69 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useApi } from "../hooks/useApi";
+import TopPageTwo from "../components/base/TopPageTwo";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 function EditMoviePage() {
     const { token } = useParams();
-    // const [movie, setMovie] = useState({});
-    const [movieId, setMovieId] = useState(0);
+    const [movie, setMovie] = useState({});
+    const [loading, setLoading] = useState(true);
     const api = useApi();
 
     useEffect(() => {
         const fetchMovieIdFromToken = async () => {
             try {
-                console.log("token " + token);
                 const res = await api(
                     '/movie-update/' + token
                 );
                 if (res && res.ok) {
                     const data = await res.json();
-                    console.log("data movie id in fetchMovieIdFromToken " + data.movie_id);
-                    console.log("data in fetchMovieIdFromToken");
-                    console.log(data);
-                    // setMovieId(data.movie_id);
-                    setMovieId(5);
-                    console.log("movieId from state " + movieId);
+                    return data.movie_id;
                 }
             } catch (e) {
                 console.error('error: ', e);
             };
         }
 
-        const fetchMovie = async () => {
+        const fetchMovie = async (movieId) => {
             try {
-                console.log(movieId);
                 const res = await api(
-                    '/movie/id/' + movieId
+                    '/movies/id/' + movieId
                 );
                 if (res && res.ok) {
                     const data = await res.json();
-                    console.log(" movie data " + data);
+                    return data;
                 }
             } catch (e) {
                 console.error('error: ', e);
             }
         };
 
-
         const fetchData = async () => {
-            await fetchMovieIdFromToken();
-            console.log("plop");
-            // await fetchMovie();
+            setMovie(await fetchMovie(await fetchMovieIdFromToken()));
+            setLoading(false);
         }
         fetchData();
     }, []);
 
 
+    if (loading) {
+        return (
+            <div className="flex flex-col justify-center items-center h-screen gap-12 text-neutral-300">
+                <p>Loading...</p>
+                <AiOutlineLoading3Quarters className="animate-spin size-24" />
+            </div>
+        );
+    }
+
     return (
         <div>
-            <p></p>
+            <TopPageTwo />
+            <div className="text-white">
+                <p> test</p>
+                <p>{movie.english_title}</p>
+            </div>
+
         </div>
     );
 
