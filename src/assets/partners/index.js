a7d6cf07eea2db833f3a9fc6be3a0406d98d41a2
Karim Pierre Zennoune <karim-pierre.zennoune@laplateforme.io>
2026-05-19 14:31:33 +0200

    added partners logo to main page

---
+++ src/assets/partners/index.js
@@ -0,0 +1,26 @@
+import brooklynStoryLab from './brooklyn-story-lab.png';
+import cnc from './cnc.png';
+import creativeAi from './creative-ai.png';
+import extraCourt from './extra-court.png';
+import humec from './humec.png';
+import laPlateforme from './la-plateforme.png';
+import leProjetImagine from './le-projet-imagine.png';
+import lhc from './lhc.png';
+import maisonDuFilm from './maison-du-film.png';
+import shortForChange from './short-for-change.png';
+
+
+const partners = {
+    brooklynStoryLab,
+    cnc,
+    creativeAi,
+    extraCourt,
+    humec,
+    laPlateforme,
+    leProjetImagine,
+    lhc,
+    maisonDuFilm,
+    shortForChange
+};
+
+export default partners;
